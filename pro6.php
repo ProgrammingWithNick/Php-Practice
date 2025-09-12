b2f9076b3e5ca70ca24c22a28d36e144f57618d7
Nikhil <167402044+ProgrammingWithNick@users.noreply.github.com>
2025-09-12 20:47:05 +0530

    Add files via upload

---
+++ pro6.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "<b>While Loop:</b><br>";
+$i = 15;
+while ($i <= 20) {
+    echo $i . " ";
+    $i++;
+}
+
+echo "<br><br>";
+
+echo "<b>Do While Loop:</b><br>";
+$j = 15;
+do {
+    echo $j . " ";
+    $j++;
+} while ($j <= 20);
+?>
